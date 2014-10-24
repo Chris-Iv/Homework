40e5d5f6a1b636e4a5d92051723bf7a7ba7f025d
Chris-Iv <chris.g.ivanov@gmail.com>
2014-10-24 17:57:01 +0300

    Added High-Quality-Methods-Homework.

---
+++ High-Quality Code/High-Quality-Methods-Homework/Methods.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Methods
+{
+    public static class Methods
+    {
+        public static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Sides should be positive.");
+            }
+
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        public static string NumberToDigit(int number)
+        {
+            if (number < 0 || number > 9)
+            {
+                throw new ArgumentOutOfRangeException("Number should be in range 0...9!");
+            }
+
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+            }
+
+            return "Invalid number!";
+        }
+
+        public static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException("Elements shouldn't be empty!");
+            }
+
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i] > elements[0])
+                {
+                    elements[0] = elements[i];
+                }
+            }
+
+            return elements[0];
+        }
+
+        public static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+
+            if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+
+            if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+        }
+
+        public static bool isHorizontal(double y1, double y2)
+        {
+            return (y1 == y2);
+        }
+
+        public static bool isVertical(double x1, double x2)
+        {
+            return (x1 == x2);
+        }
+
+        public static double CalcDistance(double x1, double y1, double x2, double y2, 
+            out bool horizontal, out bool vertical)
+        {
+            horizontal = isHorizontal(y1, y2);
+            vertical = isVertical(x1, x2);
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+            
+            Console.WriteLine(NumberToDigit(5));
+            
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+            
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
